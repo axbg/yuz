7b750378ac8aaeb3124bac3d212dd15e387b703b
axbg <bisagalexstefan@gmail.com>
2020-03-24 11:50:46 +0200

    Added download-on-click and download all button

--- yuz/app/static/js/index.js
+++ yuz/app/static/js/index.js
@@ -1,7 +1,6 @@
 const dropZone = document.getElementsByClassName("drop-zone")[0];
 const dropMessage = document.getElementsByClassName("drop-message")[0];
 const form = document.getElementById("image-form");
-const photoInput = document.getElementById("id_photo");
 
 function dragOver(event) {
     event.preventDefault();
@@ -9,13 +8,13 @@ function dragOver(event) {
         dropZone.classList.add("on-drag");
         dropMessage.textContent = "Yes, yes, there";
     }
-};
+}
 
 function dragLeave(event) {
     event.preventDefault();
     dropZone.classList.remove("on-drag");
     dropMessage.textContent = "Drop your photo in the box";
-};
+}
 
 function drop(event) {
     event.preventDefault();
@@ -29,7 +28,7 @@ function drop(event) {
             dropZone.appendChild(createImage(trimmedResult, 250, 312, 10));
             fetch("/", {
                 method: "POST",
-                body: JSON.stringify({ original: trimmedResult }),
+                body: JSON.stringify({original: trimmedResult}),
                 headers: {
                     'Content-Type': 'application/json'
                 }
@@ -41,6 +40,7 @@ function drop(event) {
                         dropZone.appendChild(createImage(crop, 250, 312, 10));
                     });
 
+                    document.getElementById("download-btn").style.display = "block";
                     dropMessage.textContent = "There you go!";
                     dropZone.classList.remove("on-drag");
                     dropZone.classList.remove('drop-zone-background');
@@ -49,15 +49,15 @@ function drop(event) {
         .catch(() => {
             console.log("Error happened when encoding");
         })
-};
+}
 
 function disableDrop(event) {
     event.preventDefault();
-};
+}
 
 function disableDrag(event) {
     event.preventDefault();
-};
+}
 
 function base64encoder(file) {
     return new Promise((resolve, reject) => {
@@ -70,7 +70,7 @@ function base64encoder(file) {
             reject();
         };
     });
-};
+}
 
 function createImage(b64, width, height, margin) {
     const image = document.createElement("img");
@@ -78,11 +78,31 @@ function createImage(b64, width, height, margin) {
     image.width = width;
     image.height = height;
     image.style.margin = margin;
-    return image;
-};
+
+    const a = document.createElement("a");
+    a.href = image.src;
+    a.classList.add("downloadable");
+    a.download = "yuz_result.png";
+    a.appendChild(image);
+    return a;
+}
+
+function downloadAll() {
+    const images = document.querySelectorAll(".downloadable");
+    repeatableDownload(images, 0, 0);
+}
+
+function repeatableDownload(images, i, timeout) {
+    setTimeout(() => {
+        if (i < images.length) {
+            images[i].click();
+            repeatableDownload(images, ++i, 750);
+        }
+    }, timeout);
+}
 
 function removeChildren(container) {
     while (container.hasChildNodes()) {
         container.removeChild(container.childNodes[0]);
     }
-};
+}
